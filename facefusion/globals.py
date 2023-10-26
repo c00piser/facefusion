65d7abb3ff8ae1f589daafaffe05047fc702d8bc
c00piser <c00piser@users.noreply.github.com>
2023-10-26 20:28:02 +0700

    change requirement numpy version

--- facefusion/globals.py
+++ facefusion/globals.py
@@ -27,14 +27,12 @@ trim_frame_start : Optional[int] = None
 trim_frame_end : Optional[int] = None
 temp_frame_format : Optional[TempFrameFormat] = None
 temp_frame_quality : Optional[int] = None
-keep_temp : Optional[bool] = None
-# output creation
 output_image_quality : Optional[int] = None
 output_video_encoder : Optional[OutputVideoEncoder] = None
 output_video_quality : Optional[int] = None
-keep_fps : Optional[bool] = None
-skip_audio : Optional[bool] = None
-# frame processors
-frame_processors : List[str] = []
-# uis
-ui_layouts : List[str] = []
+max_memory : Optional[int] = None
+execution_providers : List[str] = []
+execution_thread_count : Optional[int] = None
+execution_queue_count : Optional[int] = None
+skip_download : Optional[bool] = None
+headless : Optional[bool] = None
